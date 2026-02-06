f0b4e250ccb17fa454f4328195f51164ddf28bba
mich-de <35846885+mich-de@users.noreply.github.com>
2026-02-06 13:30:50 +0100

    Delete download_nasheet.ps1

--- download_nasheet.ps1
+++
@@ -1,5 +0,0 @@
-$url = "https://m.media-amazon.com/images/I/61b230WF+sL._AC_SX679_.jpg"
-$dest = "d:\python\profumi\public\images\lattafa\nasheet.jpg"
-Write-Host "Downloading Nasheet image..."
-curl.exe -L -o $dest $url
-if (Test-Path $dest) { Write-Host "Download successful." } else { Write-Host "Download failed." }
